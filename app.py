509699f2081be49c506bc3dfc6f1f8e6e7a30285
Marc Gravel <gravel.m@outlook.com>
2021-09-18 12:18:38 -0700

    fizzbuzz

---
+++ app.py
@@ -0,0 +1,15 @@
+def fizzbuzz(number):
+    if(number % 15 == 0):
+        return("fizzbuzz")
+    elif(number % 3 == 0):
+        return("fizz")
+    elif(number % 5 == 0):
+        return("buzz")
+    else:
+        return(number)
+
+
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+for n in number_list:
+    print(fizzbuzz(n))
